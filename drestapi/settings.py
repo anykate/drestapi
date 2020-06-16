d8a5af16fc6d769156d6565d8ae53277d1bafd91
Aniket Aryamane <aryamane.aniket@gmail.com>
2020-06-16 20:54:54 +0530

    Adding django-debug-toolbar app

--- drestapi/settings.py
+++ drestapi/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Third party apps
+    'debug_toolbar',
     'django_extensions',
     'django_seed',
     'rest_framework',
@@ -55,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'drestapi.urls'
@@ -132,3 +134,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
